b1ee0e3cb00b676238fe9b1d571daa9434e65741
周慧 <38492745+zhouhui5116@users.noreply.github.com>
2019-11-19 11:22:51 +0800

    Update RabbitMQConfig.java

--- springboot-rabbitmq/src/main/java/com/zhh/config/RabbitMQConfig.java
+++ springboot-rabbitmq/src/main/java/com/zhh/config/RabbitMQConfig.java
@@ -1,5 +1,7 @@
 package com.zhh.config;
 
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +16,13 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class RabbitMQConfig {
+    @Bean
+    public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(producerJackson2MessageConverter());
+        return rabbitTemplate;
+    }
+
     // 定义转换器，将数据序列化为JSON格式传输
     @Bean
     public MessageConverter messageConverter() {
